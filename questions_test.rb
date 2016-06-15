c943f82833168f7523bc1ec60921cf8e112f49e6
Nathan <nathangelliott@gmail.com>
2016-06-15 19:22:41 +0000

    Updated testing comments

--- questions_test.rb
+++ questions_test.rb
@@ -2,5 +2,9 @@ require 'minitest/autorun'
 require_relative 'questions'
 
 class TestQuestions < Minitest::Unit::TestCase
+  # To be implemented to insure that questions
+  # are given to the user
 
+  # questions are split as evenly as possible
+  # among the strands and standards 
 end
