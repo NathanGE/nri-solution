d4dd3254d4fa9cb5f7785b937848268ec5d97184
Nathan <nathangelliott@gmail.com>
2016-06-15 19:10:49 +0000

    Create strands with standards and questions

--- questions.rb
+++ questions.rb
@@ -1,11 +1,24 @@
 # Program for prompting users to request questions and return
 # question ID's.
-puts "How many questions would you like?"
-num_questions = gets.chomp
+# puts "How many questions would you like?"
+# num_questions = gets.chomp
 
-if num_questions.to_i <= 0
-  puts "Invalid. Please enter a whole number greater than 0"
-  num_questions = gets.chomp
-end
+# if num_questions.to_i <= 0
+#   puts "Invalid. Please enter a whole number greater than 0"
+#   num_questions = gets.chomp
+# end
 
-num_questions.to_i
+# num_questions.to_i
+
+# Since numerical id's are keys, 
+# create a multidimensional array rather than a hash
+# with 2 strands, holding 3 standards each, holding 12 questions
+standard1 = [1, 2]
+standard2 = [3]
+standard3 = [4, 5, 6]
+standard4 = [7, 8]
+standard5 = [9, 10, 11]
+standard6 = [12]
+
+strand1 = [standard1, standard2, standard3]
+strand2 = [standard4, standard5, standard6]
